f797d0d3ae8af902a79c9651f5bd074a1476d16d
Dianne <dianne.diazm@gmail.com>
2020-10-23 19:27:29 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Cuenta/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cuenta from './components/Cuenta.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HCuenta
   }
 }
 </script>
